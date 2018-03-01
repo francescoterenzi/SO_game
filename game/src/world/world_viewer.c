ac3280bd90d91959384d50a0d6b3f89493c67a64
Francesco Terenzi <terenzi_francesco@yahoo.it>
2018-03-01 15:01:50 +0100

    world_viewer bug resolved

--- game/src/world/world_viewer.c
+++ game/src/world/world_viewer.c
@@ -3,15 +3,37 @@
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
-#include "world_viewer.h"
 #include <unistd.h>
+
+#include "world_viewer.h"
 #include "../image/image.h"
 #include "../surface/surface.h"
 
 int window;
 
+typedef enum ViewType {Inside, Outside, Global} ViewType;
+
+typedef struct WorldViewer{
+  World* world;
+  float zoom;
+  float camera_z;
+  int window_width, window_height;
+  Vehicle* self;
+  ViewType view_type;
+} WorldViewer;
 
 WorldViewer viewer;
+
+void WorldViewer_run(WorldViewer* viewer,
+		     World* world,
+		     Vehicle* self,
+		     int* argc, char** argv);
+
+void WorldViewer_draw(WorldViewer* viewer);
+
+void WorldViewer_destroy(WorldViewer* viewer);
+
+void WorldViewer_reshapeViewport(WorldViewer* viewer, int width, int height);
 
 
 void keyPressed(unsigned char key, int x, int y)
@@ -234,8 +256,24 @@ void Surface_draw(Surface* s) {
 }
 
 
+void Vehicle_applyTexture(Vehicle* v) {
+  //printf("applying texture %p to vehicle %p\n", v->texture, v);
+  v->_destructor=Vehicle_destructor;
+  if (v->gl_list>-1)
+    glDeleteLists(v->gl_list, 1);
+  v->gl_list = -1;
+  if (v->gl_texture>-1)
+    glDeleteTextures(1, (unsigned int*) &v->gl_texture);
+  v->gl_texture = -1;
+  if (v->texture)
+    v->gl_texture=Image_toTexture(v->texture);
+}
+
 void Vehicle_draw(Vehicle* v){
   if (v->gl_list<0){
+    if (v->texture) {
+      Vehicle_applyTexture(v);
+    }
     v->gl_list = glGenLists(1);
     glNewList(v->gl_list, GL_COMPILE);
     if (v->gl_texture>-1){
@@ -256,20 +294,6 @@ void Vehicle_draw(Vehicle* v){
   glPopMatrix();
 }
 
-
-void Vehicle_applyTexture(Vehicle* v, Image* img) {
-  v->texture=img;
-  //printf("applying texture %p to vehicle %p\n", img, v);
-  v->_destructor=Vehicle_destructor;
-  if (v->gl_list>-1)
-    glDeleteLists(v->gl_list, 1);
-  v->gl_list = -1;
-  if (v->gl_texture>-1)
-    glDeleteTextures(1, (unsigned int*) &v->gl_texture);
-  v->gl_texture = -1;
-  if (img)
-    v->gl_texture=Image_toTexture(img);
-}
 
 void WorldViewer_init(WorldViewer* viewer,
 		      World* w,
@@ -296,7 +320,7 @@ void WorldViewer_init(WorldViewer* viewer,
   ListItem* item=viewer->world->vehicles.first;
   while(item){
     Vehicle* v=(Vehicle*)item;
-    Vehicle_applyTexture(v, v->texture);
+    Vehicle_applyTexture(v);
     if (v==self){
       viewer->self=self;
     }
